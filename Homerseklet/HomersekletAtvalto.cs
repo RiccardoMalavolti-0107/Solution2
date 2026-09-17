b056647b89a574a9e8b7db1d11b1d5c5bec915e3
RiccardoMalavolti-010 <malavolti.riccardo.2022d@telekimezotur.hu>
2026-09-17 09:05:03 +0200

    homerseklet

--- Homerseklet/HomersekletAtvalto.cs
+++ Homerseklet/HomersekletAtvalto.cs
@@ -6,9 +6,14 @@ namespace Homerseklet
 {
     internal class HomersekletAtvalto
     {
-        public int Celsius { get; set; };
+        public int Celsius { get; set; }
 
-        public int ToFahrenheit(int celsius)
+        public int ToFahrenheit()
+        {
+            return (10 * 9 / 5) + 32;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
         {
             return (celsius * 9 / 5) + 32;
         }
--- Homerseklet/Program.cs
+++ Homerseklet/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Homerseklet;
+
+HomersekletAtvalto homerseklet = new HomersekletAtvalto();
+homerseklet.Celsius = 25;
+
+int fahrenheit = homerseklet.ToFahrenheit();
+int fahrenheitStatic = (int)HomersekletAtvalto.CelsiusToFahrenheit(homerseklet.Celsius);
+
+Console.WriteLine(fahrenheit);
+Console.WriteLine(fahrenheitStatic);
