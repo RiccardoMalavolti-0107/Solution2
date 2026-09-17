cd73cf96234fb78162b2bbfb7dcc9b107ccd3ab2
RiccardoMalavolti-010 <malavolti.riccardo.2022d@telekimezotur.hu>
2026-09-17 09:21:30 +0200

    DiakApp

---
+++ DiakApp/Diak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiakApp
+{
+    internal class Diak
+    {
+        public string Nev { get; set; }
+        public double Atlag { get; set; }
+
+        public Diak()
+        {
+            Nev = "Hokhi Zoltán";
+            Atlag = 5.0;
+        }
+        public Diak(string nev,double atlag)
+        {
+            Nev = nev;
+            Atlag = atlag;
+            
+        }
+        public Diak(double atlag)
+        {
+            Nev = "Betherp Eszter";
+            Atlag = 4.0;
+        }
+    }
+}
---
+++ DiakApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using DiakApp;
+
+Diak ParameterNelkul = new Diak();
+Diak EgyParameteres = new Diak(4.0);
+Diak KetParameteres = new Diak("Tudhou R. Ákos",1.0);
+
+EgyParameteres.Atlag = 4.0;
+Console.WriteLine(ParameterNelkul.Nev + " " + ParameterNelkul.Atlag);
+Console.WriteLine(EgyParameteres.Nev + " " + EgyParameteres.Atlag);
+Console.WriteLine(KetParameteres.Nev + " " + KetParameteres.Atlag);
