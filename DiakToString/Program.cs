b7818abe76e1858ba70e56f2feb145d83acda513
RiccardoMalavolti-010 <malavolti.riccardo.2022d@telekimezotur.hu>
2026-09-17 09:32:42 +0200

    DiakToString

---
+++ DiakToString/Program.cs
@@ -0,0 +1,9 @@
+﻿using DiakApp;
+
+Diak diak1 = new Diak(4.11);
+Diak diak2 = new Diak("Tudhou Robert Akos", 1.2);
+Diak diak3 = new Diak();
+
+Console.WriteLine(diak1.ToString());
+Console.WriteLine(diak2.ToString());
+Console.WriteLine(diak3.ToString());
