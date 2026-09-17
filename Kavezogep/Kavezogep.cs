e70aa6a1b851381f4c9c837f32c9b31b79064c82
RiccardoMalavolti-010 <malavolti.riccardo.2022d@telekimezotur.hu>
2026-09-17 09:05:13 +0200

    kavezogep

---
+++ Kavezogep/Kavezogep.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kavezogep
+{
+    public class Kavezogepek
+    {
+        private int VizKeszletMl { get; set; }
+        public void VizTolt(int ml)
+        {
+            VizKeszletMl += ml;
+            Console.WriteLine("{0} viz betoltve",ml);
+
+        }
+        public void KaveFozes()
+        {
+            if (VizKeszletMl >= 150)
+            {
+                VizKeszletMl -= 150;
+                Console.WriteLine("1 db kave lefozve");
+                Console.WriteLine("{0} vizkeszlet maradt", VizKeszletMl);
+            }
+            else
+            {
+                Console.WriteLine("nincs eleg viz");
+            }
+        }
+        public void KaveFozes(int db)
+        {
+            if (VizKeszletMl >= 150*db)
+            {
+                VizKeszletMl -= 150*db;
+                Console.WriteLine("{0}db kave lefozve", db);
+                Console.WriteLine("{0} vizkeszlet maradt", VizKeszletMl);
+            }
+            else
+            {
+                Console.WriteLine("nincs eleg viz");
+            }
+        }
+    }
+}
---
+++ Kavezogep/Program.cs
@@ -0,0 +1,6 @@
+﻿using Kavezogep;
+Kavezogepek kavezogep = new Kavezogep.Kavezogepek();
+
+kavezogep.VizTolt(1650);
+kavezogep.KaveFozes();
+kavezogep.KaveFozes(10);
